df8055a39f9b38ef5c091a7f650070679f0ea67f
ArrayZoneYour <hustliyidong@gmail.com>
2021-05-08 23:22:29 +0800

    :lipstick: (progress) reuse the same bar for the same task

--- progress.ts
+++ progress.ts
@@ -17,33 +17,41 @@ class Progress {
   public phase = 'Computing table 0'
   public id = ''
   public tmpPath = ''
-  private bar = new SingleBar({})
+  private bar: SingleBar | undefined
   private start = new Date().toLocaleString()
 
   public create(name: string, total = 128) {
     const sourcePathRoute = this.tmpPath.split('/')
-    this.bar = multibar.create(total, 0, {
-      name,
-      source: sourcePathRoute[sourcePathRoute.length - 1],
-      start: this.start
-    })
+    if (!this.bar) {
+      this.bar = multibar.create(total, 0, {
+        name: name.padEnd(26),
+        source: sourcePathRoute[sourcePathRoute.length - 1],
+        start: this.start
+      })
+    } else {
+      this.bar.update(0)
+      this.bar.setTotal(total)
+      this.bar.update(0, {
+        name: name.padEnd(26)
+      })
+    }
   }
 
   public refresh() {
-    this.bar.render()
+    this.bar?.render()
   }
 
   public update(num: number) {
-    this.bar.update(num)
+    this.bar?.update(num)
   }
 
   public increment() {
-    this.bar.increment()
+    this.bar?.increment()
   }
 
   public stop() {
-    this.bar.stop()
-    multibar.remove(this.bar)
+    this.bar?.stop()
+    // multibar.remove(this.bar)
   }
 }
 
